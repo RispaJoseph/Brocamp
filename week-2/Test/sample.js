e202ad8853892dbbda04ebdd4e084da7523050df
RispaJoseph <rizpahjoseph@gmail.com>
2023-08-21 09:26:21 +0530

    variables and datatypes in js

---
+++ week-2/Test/sample.js
@@ -0,0 +1,54 @@
+document.write("Hello World!");
+
+document.write("<h1>Learn JavaScriprt</h1>");
+
+// alert("Hello world!");
+
+// window.alert("Hello");
+
+console.log("Nice to meet you, Why are you here?");
+
+document.write("<hr>");
+
+
+// ..............................................................
+
+document.write("Variables and Datatypes","<br>");
+
+// string
+var person1 = "Rispa";
+let person2 = "Joseph";
+document.write(person1,"<br>",person2,"<br>");
+document.write(typeof person1,"<br>");
+
+// Nunber
+var num1 = 10;
+var num2 = 56.589;
+document.write(num1,"<br>",num2,"<br>");
+document.write(typeof num2,"<br>");
+
+// Boolean
+var js = true;
+var html = false;
+document.write(js,"<br>",html,"<br>");
+
+// undefined
+var test1;
+document.write(test1,"<br>");
+
+
+// Null
+var test2 = null;
+document.write(test2,"<br>");
+
+// example
+var a=10; b=40;
+document.write(a + b  ,"<br>");
+
+
+// Constant
+
+const pi= 3.14;
+document.write(pi);
+pi = 3;
+document.write(pi);
