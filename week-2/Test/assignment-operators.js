612ba80f5ab0f9bdd8c99e6734114f8f494d90e3
RispaJoseph <rizpahjoseph@gmail.com>
2023-08-21 10:12:53 +0530

    Assignment Operators

---
+++ week-2/Test/assignment-operators.js
@@ -0,0 +1,23 @@
+document.write("<hr>");
+document.write("Assignment Operators");
+document.write("<br>");
+var x = 10;
+
+// Add and Assign
+document.write(x+=5);
+document.write("<br>");
+
+document.write(x-=5);
+document.write("<br>");
+
+document.write(x*=5);
+document.write("<br>");
+
+document.write(x/=5);
+document.write("<br>");
+
+document.write(x%=5);
+document.write("<br>");
+
+document.write(x+=5);
+document.write("<br>");
