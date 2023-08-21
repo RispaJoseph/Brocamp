5432330e0e07d9448a2446c97385cc548a0ab937
RispaJoseph <rizpahjoseph@gmail.com>
2023-08-21 10:03:46 +0530

    arithmetic operators in js

---
+++ week-2/Test/arithmetic-operators.js
@@ -0,0 +1,37 @@
+var a = 100;
+var b = 50;
+
+document.write("<hr> Arithmetic Operators <br>");
+document.write(a + b);
+document.write("<br>");
+
+document.write(a - b);
+document.write("<br>");
+
+document.write(a / b);
+document.write("<br>");
+
+
+document.write(a * b);
+document.write("<br>");
+
+var a = 20;
+var b=3;
+document.write(a % b);
+document.write("<br>");
+
+
+a++;
+document.write(a);
+document.write("<br>");
+
+b--;
+document.write(b);
+document.write("<br>");
+
+
+// exponentiation operator
+document.write(5 ** 2);
+document.write("<br>");
+document.write(2 ** 3);
+document.write("<br>");
