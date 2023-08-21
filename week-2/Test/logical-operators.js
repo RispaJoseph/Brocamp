03c5a61cfe68466ab649a1301aee22c2c6af499a
RispaJoseph <rizpahjoseph@gmail.com>
2023-08-21 12:09:36 +0530

    Logical Operators

---
+++ week-2/Test/logical-operators.js
@@ -0,0 +1,18 @@
+document.write("<hr>");
+document.write("Logical Operator");
+document.write("<br>");
+// logical AND
+var a = 50;
+var b = 40;
+document.write(a == b);
+document.write("<br>");
+
+
+// logical OR
+document.write(a ==10 || b==40 );
+document.write("<br>");
+
+
+// logical NOT
+document.write(!(a==50));
+document.write("<br>");
